7f783fd6f86783fb3d4c60b1f60e8c51ea1ea8aa
chanjin_ahn <olkkani@gmail.com>
2024-11-22 21:32:06 +0900

    Fix postgrsql Driver class error

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,8 +44,7 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     // Database
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-    implementation("io.r2dbc:r2dbc-postgresql")
-    runtimeOnly("org.postgresql:r2dbc-postgresql")
+    implementation("org.postgresql:r2dbc-postgresql")
     runtimeOnly("org.postgresql:postgresql")
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
