63e37a6023556f539038cdaab56a6bfc16a223e7
chanjin_ahn <olkkani@gmail.com>
2024-11-22 22:12:42 +0900

    delete not use dependency, kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("io.kotest:kotest-runner-junit5")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
