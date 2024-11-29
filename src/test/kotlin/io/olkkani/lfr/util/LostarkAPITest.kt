9bad6a43c36e5966c98d3abdda1de584d5999da6
acj <ahn.chanjin.96@gmail.com>
2024-11-30 01:13:25 +0900

    delete error code

--- src/test/kotlin/io/olkkani/lfr/util/LostarkAPITest.kt
+++ src/test/kotlin/io/olkkani/lfr/util/LostarkAPITest.kt
@@ -2,7 +2,6 @@ package io.olkkani.lfr.util
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.olkkani.lfr.api.LostarkAPIClient
-import io.olkkani.lfr.model.AuctionRequest
 import org.junit.jupiter.api.Test
 
 class LostarkAPITest (
@@ -14,13 +13,5 @@ class LostarkAPITest (
     @Test
     fun `API_가져오기_TEST` (){
         // given
-        val request = AuctionRequest(itemName = "10레벨 멸화의 보석", itemTier = 3, itemGrade = "유물", itemCode = 65021100)
-        apiClient.getAuctionItems2(request)
-
-        apiClient.getAuctionItems(request).subscribe({response ->
-            response.items.forEach { item ->
-                logger.error { item.auctionInfo.buyPrice }
-            }
-        })
     }
 }
