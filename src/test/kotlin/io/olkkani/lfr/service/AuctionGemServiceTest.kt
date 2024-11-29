df037ec7a7d98d70badb88f17151bc98ac5ff3bf
acj <ahn.chanjin.96@gmail.com>
2024-11-30 01:19:40 +0900

    delete error code

--- src/test/kotlin/io/olkkani/lfr/service/AuctionGemServiceTest.kt
+++ src/test/kotlin/io/olkkani/lfr/service/AuctionGemServiceTest.kt
@@ -1,16 +1,14 @@
 package io.olkkani.lfr.service
 
-import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class AuctionGemServiceTest (
-    @Autowired private val service: AuctionService
+//    @Autowired private val service: AuctionService
 ){
 
-    @Test
-    fun `11`(){
-
-    }
+//    @Test
+//    fun `11`(){
+//
+//    }
 }
