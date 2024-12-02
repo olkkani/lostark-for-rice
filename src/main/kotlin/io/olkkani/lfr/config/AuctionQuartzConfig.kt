63f7e6f664ce2d1ed844c1ef4573bd872111176e
acj <ahn.chanjin.96@gmail.com>
2024-12-02 17:49:54 +0900

    fix quartz config bean create error

--- src/main/kotlin/io/olkkani/lfr/config/AuctionQuartzConfig.kt
+++ src/main/kotlin/io/olkkani/lfr/config/AuctionQuartzConfig.kt
@@ -4,21 +4,20 @@ import io.olkkani.lfr.scheduler.GemOpenPricesRetrievalJob
 import io.olkkani.lfr.scheduler.GemPricesRetrievalJob
 import io.olkkani.lfr.scheduler.SaveTodayPricesJob
 import org.quartz.*
-import org.quartz.impl.StdSchedulerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.scheduling.quartz.SchedulerFactoryBean
 
 @Configuration
 @Profile("prod")
 class AuctionQuartzConfig {
-
     @Bean
-    fun scheduler(): Scheduler {
-        val scheduler = StdSchedulerFactory().scheduler
-        scheduler.start()
-        return scheduler
-    }
+    fun schedulerFactoryBean(triggers: Array<Trigger>, jobDetails: Array<JobDetail>): SchedulerFactoryBean =
+        SchedulerFactoryBean().apply {
+            setTriggers(*triggers)
+            setJobDetails(*jobDetails)
+        }
 
     @Bean
     fun gemOpenPricesRetrievalJobDetail(): JobDetail {
@@ -45,63 +44,40 @@ class AuctionQuartzConfig {
     }
 
     @Bean
-    fun gemPriceRetrievalTriggerDaily0010(): List<Trigger> {
-        val cronExpression1 = "0 10 0 ? * MON,TUE,THU,FRI,SAT,SUN"
-        val cronExpression2 = "0 10 10 ? * WED"
-
-        val dailyExceptWedTrigger = TriggerBuilder.newTrigger()
+    fun fetchGemOpenPriceTriggerDailyWithoutWed(): Trigger =
+        TriggerBuilder.newTrigger()
+            .withIdentity("trigger-fetchGemPriceTriggerDaily0010WithWed")
+            .withSchedule(CronScheduleBuilder.cronSchedule("0 10 0 ? * MON,TUE,THU,FRI,SAT,SUN"))
             .forJob(gemOpenPricesRetrievalJobDetail())
-            .withIdentity("trigger-daily0010")
-            .withSchedule(CronScheduleBuilder.cronSchedule(cronExpression1))
             .build()
-
-        val wednesdayTrigger = TriggerBuilder.newTrigger()
+    @Bean
+    fun fetchGemOpenPriceTriggerWed(): Trigger =
+        TriggerBuilder.newTrigger()
+            .withIdentity("trigger-fetchGemPriceTriggerWed1010")
+            .withSchedule(CronScheduleBuilder.cronSchedule("0 10 10 ? * WED"))
             .forJob(gemOpenPricesRetrievalJobDetail())
-            .withIdentity("trigger-web1010")
-            .withSchedule(CronScheduleBuilder.cronSchedule(cronExpression2))
             .build()
-
-        scheduler().scheduleJob(dailyExceptWedTrigger)
-        scheduler().scheduleJob(wednesdayTrigger)
-
-        return listOf(dailyExceptWedTrigger, wednesdayTrigger)
-    }
 
     @Bean
-    fun gemPriceRetrievalTriggerEvery2HoursWedExcluding6to10(): List<Trigger> {
-        val cronExpression1 = "0 0 1-23/2 ? * MON,TUE,THU,FRI,SAT,SUN"
-        val cronExpression2 = "0 0 1-5,11-23/2 ? * WED"
-
-        val everyOddHourExceptWednesdayTrigger = TriggerBuilder.newTrigger()
-            .forJob(gemPricesRetrievalJobDetail())
+    fun fetchGemPriceTriggerDailyWithoutWed(): Trigger =
+        TriggerBuilder.newTrigger()
             .withIdentity("trigger-everyOddHourExceptWed")
-            .withSchedule(CronScheduleBuilder.cronSchedule(cronExpression1))
-            .build()
-
-        val wednesdayBiHourlyNightTrigger = TriggerBuilder.newTrigger()
+            .withSchedule(CronScheduleBuilder.cronSchedule("0 0 1-23/2 ? * MON,TUE,THU,FRI,SAT,SUN"))
             .forJob(gemPricesRetrievalJobDetail())
-            .withIdentity("trigger-WednesdayBiHourlyNight")
-            .withSchedule(CronScheduleBuilder.cronSchedule(cronExpression2))
             .build()
-
-        scheduler().scheduleJob(everyOddHourExceptWednesdayTrigger)
-        scheduler().scheduleJob(wednesdayBiHourlyNightTrigger)
-
-        return listOf(everyOddHourExceptWednesdayTrigger, wednesdayBiHourlyNightTrigger)
-    }
+    @Bean
+    fun fetchGemPriceTriggerWed(): Trigger =
+        TriggerBuilder.newTrigger()
+            .withIdentity("trigger-WednesdayBiHourlyNight")
+            .withSchedule(CronScheduleBuilder.cronSchedule("0 0 1-5,11-23/2 ? * WED"))
+            .forJob(gemPricesRetrievalJobDetail())
+            .build()
 
     @Bean
-    fun saveGemPriceTriggerDaily2350(): Trigger {
-        val cronExpression = "0 50 23 ? * *"
-
-        val everyNightTrigger = TriggerBuilder.newTrigger()
-            .forJob(saveTodayPricesJobDetail())
+    fun saveGemPriceTrigger(): Trigger =
+        TriggerBuilder.newTrigger()
             .withIdentity("trigger-everyNight")
-            .withSchedule(CronScheduleBuilder.cronSchedule(cronExpression))
+            .withSchedule(CronScheduleBuilder.cronSchedule("0 50 23 ? * *"))
+            .forJob(saveTodayPricesJobDetail())
             .build()
-
-        scheduler().scheduleJob(everyNightTrigger)
-
-        return everyNightTrigger
-    }
 }
