86eb0db4b6ccf3be1ea7340bd0b8614c73480437
acj <ahn.chanjin.96@gmail.com>
2025-02-07 18:28:43 +0900

    CamelCaseToUnderscoresNamingStrategy 로 변경

--- src/main/kotlin/io/olkkani/lfr/entity/jpa/ItemPriceIndex.kt
+++ src/main/kotlin/io/olkkani/lfr/entity/jpa/ItemPriceIndex.kt
@@ -1,29 +1,20 @@
 package io.olkkani.lfr.entity.jpa
 
 import io.hypersistence.utils.hibernate.id.Tsid
-import jakarta.persistence.Column
-import jakarta.persistence.ColumnResult
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import java.time.LocalDate
 
 @Entity
-@Table(name = "item_price_index")
 class ItemPriceIndex (
     @Id @Tsid
     val id: Long? = null,
-    @Column
     val itemCode: Int? = null,
-    @Column
     val recordedDate: LocalDate,
-    @Column
     var closePrice: Int,
-    @Column
     var openPrice: Int,
-    @Column
     var highPrice: Int,
-    @Column
     var lowPrice: Int,
 )
 
