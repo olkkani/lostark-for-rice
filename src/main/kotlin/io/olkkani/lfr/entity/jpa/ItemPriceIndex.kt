5a03fcadccf6a21d731444bd8a4b058098ee88aa
acj <ahn.chanjin.96@gmail.com>
2025-02-07 18:47:55 +0900

    Column 매핑을 명시적으로 지정

--- src/main/kotlin/io/olkkani/lfr/entity/jpa/ItemPriceIndex.kt
+++ src/main/kotlin/io/olkkani/lfr/entity/jpa/ItemPriceIndex.kt
@@ -1,20 +1,29 @@
 package io.olkkani.lfr.entity.jpa
 
 import io.hypersistence.utils.hibernate.id.Tsid
+import jakarta.persistence.Column
+import jakarta.persistence.ColumnResult
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import java.time.LocalDate
 
 @Entity
+@Table(name = "item_price_index")
 class ItemPriceIndex (
     @Id @Tsid
     val id: Long? = null,
+    @Column(name = "item_code")
     val itemCode: Int? = null,
+    @Column(name = "recorded_date")
     val recordedDate: LocalDate,
+    @Column(name = "close_price")
     var closePrice: Int,
+    @Column(name = "open_price")
     var openPrice: Int,
+    @Column(name = "high_price")
     var highPrice: Int,
+    @Column(name = "low_price")
     var lowPrice: Int,
 )
 
