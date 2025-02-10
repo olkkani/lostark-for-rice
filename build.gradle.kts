ee0c85312f2725e88cf861e4f9621d383daf5804
acj <ahn.chanjin.96@gmail.com>
2025-02-10 10:56:46 +0900

    fix service annotation error

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,10 +62,10 @@ dependencies {
             }
         }
     }
+    developmentOnly("de.flapdoodle.embed:de.flapdoodle.embed.mongo.spring3x:4.18.0")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:5.9.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo.spring3x:4.18.0")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.9.25")
--- src/main/kotlin/io/olkkani/lfr/service/GemPriceServiceImpl.kt
+++ src/main/kotlin/io/olkkani/lfr/service/GemPriceServiceImpl.kt
@@ -5,8 +5,10 @@ import io.olkkani.lfr.entity.jpa.ItemPriceIndex
 import io.olkkani.lfr.entity.mongo.ItemPriceIndexTrend
 import io.olkkani.lfr.repository.jpa.ItemPriceIndexRepository
 import io.olkkani.lfr.repository.mongo.ItemPriceIndexTrendRepository
+import org.springframework.stereotype.Service
 import java.time.LocalDate
 
+@Service
 class GemPriceServiceImpl(
     private val indexRepository: ItemPriceIndexRepository,
     private val indexTrendRepository: ItemPriceIndexTrendRepository,
