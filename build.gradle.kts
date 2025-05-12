44b0059f0183dc8ad503a8a756f96850233ae634
acj <ahn.chanjin.96@gmail.com>
2025-05-12 20:19:23 +0900

    add redis dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,30 +31,19 @@ dependencies {
     // Spring
     implementation(libs.bundles.spring) { exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat") }
     kapt("org.springframework.boot:spring-boot-configuration-processor")
-    implementation(libs.spring.quartz)
     developmentOnly(libs.spring.devtools)
     // DB
-    implementation(libs.mongodb)
-    implementation(libs.postgresql)
-    implementation(libs.hibernate.utils)
-
+    implementation(libs.bundles.persistence)
     // Test and Logging
     implementation(libs.kotlin.logging)
-    testImplementation(libs.embed.mongo)
+    implementation(libs.p6spy)
+    testImplementation(libs.bundles.test)
     testImplementation(libs.bundles.spring.test)
-    testImplementation(libs.kotest.junit.runner)
-    testImplementation(libs.mockk)
-    testImplementation(libs.reactor.test)
-    testImplementation(libs.kotest.extensions.spring)
-    testImplementation(libs.junit.kotlin)
-    testRuntimeOnly(libs.junit.launcher)
-    runtimeOnly(libs.h2)
-
+    developmentOnly(libs.bundles.persistence.database.embedded)
+    testImplementation(libs.bundles.persistence.database.embedded)
     // the other
     implementation(libs.jackson.kotlin)
     implementation(libs.reactor.kotlin.extensions)
-    implementation(libs.tsid.creator)
-    implementation(libs.p6spy)
     implementation(libs.commons.text)
     implementation(libs.coroutines.core)
     implementation(libs.coroutines.reactor)
