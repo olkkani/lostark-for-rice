ab04c7315ab0a45e46c87b2721e15dd1373c3cd1
acj <ahn.chanjin.96@gmail.com>
2025-05-14 17:54:27 +0900

    enable flyway and init

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,11 +29,25 @@ repositories {
 
 dependencies {
     // Spring
-    implementation(libs.bundles.spring) { exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat") }
+    implementation(libs.bundles.spring) {
+        exclude(
+            group = "org.springframework.boot",
+            module = "spring-boot-starter-tomcat"
+        )
+    }
     kapt("org.springframework.boot:spring-boot-configuration-processor")
     developmentOnly(libs.spring.devtools)
     // DB
     implementation(libs.bundles.persistence)
+    implementation(libs.bundles.persistence.database)
+
+//    val jooqVersion = "3.20.3" // jooq {} 블록에 설정된 버전과 일치시킵니다.
+//    jooqGenerator("org.jooq:jooq:$jooqVersion")
+//    jooqGenerator("org.jooq:jooq-meta:$jooqVersion")
+//    jooqGenerator("org.jooq:jooq-meta-extensions:$jooqVersion")
+//
+//    jooqGenerator(libs.bundles.persistence)
+//    jooqGenerator(libs.h2)
     // Test and Logging
     implementation(libs.kotlin.logging)
     implementation(libs.p6spy)
@@ -58,9 +72,11 @@ dependencies {
     }
 }
 val generated = file("build/generated/kapt/main/kotlin")
+//val jooqGenerated = file("build/generated-src/jooq/main")
 sourceSets {
     main {
         kotlin.srcDirs += generated
+//        kotlin.srcDirs += jooqGenerated
     }
 }
 tasks.named("clean") {
@@ -77,3 +93,56 @@ tasks.withType<Test>().configureEach {
     useJUnitPlatform()
     jvmArgs("-Xshare:off")
 }
+//jooq {
+//    version.set("3.20.3")  // default (can be omitted), libs.versions jooq 과 버전 통일
+//    edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)  // default (can be omitted)
+//
+//    configurations {
+//        create("main") {
+//            generateSchemaSourceOnCompilation.set(false)
+//
+//            jooqConfiguration.apply {
+//                logging = org.jooq.meta.jaxb.Logging.WARN
+//
+//                jdbc = null // JDBC 연결을 사용하지 않음 (JPA 엔티티로부터 생성)
+//
+//                generator.apply {
+//                    name = "org.jooq.codegen.KotlinGenerator" // Kotlin 코드 생성
+//                    database.apply {
+//                        name = "org.jooq.meta.extensions.jpa.JPADatabase"
+//                        properties = listOf(
+//                            org.jooq.meta.jaxb.Property().apply {
+//                                key = "packages"
+//                                value = "io.olkkani.lfr.entity.jpa" // JPA 엔티티 패키지 경로
+//                            },
+//                            org.jooq.meta.jaxb.Property().apply {
+//                                key = "useAttributeConverters"
+//                                value = "true"
+//                            },
+//                            org.jooq.meta.jaxb.Property().apply {
+//                                key = "dialect"
+//                                value = "POSTGRES" // PostgreSQL 방언 사용
+//                            }
+//                        )
+//                    }
+//
+//                    generate.apply {
+//                        isDeprecated = false
+//                        isRecords = true
+//                        isImmutablePojos = true
+//                        isFluentSetters = true
+//                        isJavaTimeTypes = true
+//                        isPojosEqualsAndHashCode = true
+//                    }
+//
+//                    target.apply {
+//                        packageName = "io.olkkani.generated.jooq" // 생성된 코드 패키지 경로
+//                        directory = "build/generated-src/jooq/main" // 생성된 코드 저장 위치
+//                    }
+//
+//                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+//                }
+//            }
+//        }
+//    }
+//}
