6b6ac840ffddd44d18b9eb052d87ae6c1daed7b9
acj <ahn.chanjin.96@gmail.com>
2025-11-13 18:24:57 +0900

    delete unused repository class

--- processor-service/src/main/kotlin/io/oikkani/processorservice/infrastructure/outbound/repository/adapter/MarketItemOhlcaRepositoryAdapter.kt
+++ processor-service/src/main/kotlin/io/oikkani/processorservice/infrastructure/outbound/repository/adapter/MarketItemOhlcaRepositoryAdapter.kt
@@ -2,7 +2,6 @@ package io.oikkani.processorservice.infrastructure.outbound.repository.adapter
 
 import io.oikkani.processorservice.application.port.outbound.MarketItemOhlcaRepositoryPort
 import io.oikkani.processorservice.infrastructure.outbound.repository.entity.DailyMarketItemOhlcaPrice
-import io.oikkani.processorservice.infrastructure.outbound.repository.jooq.DailyMarketItemOhlcaPriceJooqRepository
 import io.oikkani.processorservice.infrastructure.outbound.repository.jpa.DailyMarketItemOhlcaPriceJpaRepository
 import org.springframework.stereotype.Component
 import java.time.LocalDate
@@ -10,7 +9,6 @@ import java.time.LocalDate
 @Component
 class MarketItemOhlcaRepositoryAdapter(
     private val jpaRepository: DailyMarketItemOhlcaPriceJpaRepository,
-    private val jooqRepository: DailyMarketItemOhlcaPriceJooqRepository
 ): MarketItemOhlcaRepositoryPort {
     override fun save(ohlcPrice: DailyMarketItemOhlcaPrice) {
         jpaRepository.save(ohlcPrice)
